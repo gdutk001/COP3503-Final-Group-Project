9c9c6200f469fb57e14c3b6efb8b86053b3881a6
gdutk001 <gdutk001@ufl.edu>
2016-12-04 13:01:11 -0500

    Update Final.h

--- Final.h
+++ Final.h
@@ -5,20 +5,92 @@
 #ifndef main_h
 #define main_h
 
+#include <vector>
 using namespace std;
 
-/*class ATM {
+class ATM {
 private:
+	//need to initialize allCustomers
 	vector<Customer> allCustomers;
 	Person currentCustomer;
+	int findCustomer(int cardNumber){
+		//iterates through allCustomers comparing cardNumbers until it finds a match
+		for (int i = 0; i < allCustomers.size(); i++) {
+			if (allCustomers[i].compareCardNumber(cardNumber)){
+				return i;
+			}
+		}
+
+		//returns -1 if no match is found
+		return -1;
+	}
 
 public:
 	bool addNewCustomer();
-	bool findCustomer(int cardNumber);
-	bool checkPIN(int PIN);
+
+	//finish method below
+	bool setCurrentCustomer(int cardNumber){
+		//makes sure cardNumber is that of valid Customer
+		x = findCustomer(cardNumber);
+		if (x < 0) {
+			return false;
+		}
+
+		int PIN;
+
+		//Gaurd for entering a PIN
+    int counter;
+    int attempts;
+    attempts = 3;
+    while(counter < 1 ){
+      cout << "Please enter your PIN"
+      cin >> PIN;
+    // checks that user input is an integer
+      while(std::cin.fail()) {
+              cout << "Error, please input a valid pin number" << std::endl;
+              cin.clear();
+              cin.ignore(256,'\n');
+              cin >> PIN;
+          }
+      // Checks for correct PIN number
+        if(comparePIN(PIN)){
+          counter++;
+        } else{
+          // decrements one attempt and asks user to try again
+               if(attempts == 0){
+                 //If PIN number is false and user ran out of tries program is exited
+               cout << "You have ran out of tries, please login again"
+               exit(0);
+                }
+          attempts--;
+          cout << "Incorret input, please input your PIN. You have " << attempts << " attempts remaining" << endl;
+        }
+      }
+      attempts = 3;
+		//add possible loops for missed trys
+		//checks that user knows PIN for cardNumber
+		if (allCustomers[x].comparePIN(PIN)){
+			currentCustomer = allCustomers[x];
+			return true;
+		}
+
+		return false;
+	}
+
+	Customer getCustomer(int cardNumber){
+		x = findCustomer(cardNumber);
+
+		//if x is within the size of allCustomers returns the customer at that location
+		if (x > -1) {
+			return allCustomers[x];
+		}
+
+		//otherwise returns nullptr reference (NULL)
+		return nullptr;
+	}
 
 };
-*/
+
 
 
 class Customer {
@@ -45,29 +117,58 @@ public:
 		int enteredPIN = -1;
 
 		while (enteredPIN != 0) {
+        int i;
+        while(i<0){
+
 			cout << "Enter your current PIN (enter 0 to cancel): ";
 			cin >> enteredPIN;
+      while(std::cin.fail()) {
+              cout << "Error, please input a valid pin number" << std::endl;
+              cin.clear();
+              cin.ignore(256,'\n');
+              cin >> enteredPIN;
+          }
 			if (comparePIN(enteredPIN)) {
-				
+
+				//cin needs a guard against invalid input
+
 				//accepts 2 user inputed PIN's to make sure new PIN is desired PIN
 				cout << "Please enter new PIN: ";
 				cin >> enteredPIN;
+        while(std::cin.fail()) {
+                cout << "Error, please input a number" << std::endl;
+                cin.clear();
+                cin.ignore(256,'\n');
+                cin >> enteredPIN;
+            }
 				int newPIN;
 				cout << "Please re-enter new PIN: ";
 				cin >> newPIN;
-
+        while(std::cin.fail()) {
+                cout << "Error, please input a number" << std::endl;
+                cin.clear();
+                cin.ignore(256,'\n');
+                cin >> newPIN;
+            }
 				//if 2 entered PIN's are same, set new PIN
 				if (newPIN == enteredPIN) {
 					this->PIN = newPIN;
 					return true;
+          i++
 				}
+
+				//possibly add a loop
 				else {
 					cout << "Entered PINS do not match. Please try again";
-					
+
 				}
 			}
 
+
 		}
+
+  }
+
 	}
 
 	bool comparePIN(int PIN) {
@@ -106,7 +207,20 @@ public:
 		return this->balance;
 	}
 
-	bool transfer(int cardNumber, double x);
+	bool transfer(int cardNumber, double x, ATM atm){
+
+		Customer transferTo = atm.getCustomer(cardNumber);
+
+		//attempts to make withdrawal, if there are sufficient funds, makes deposit in other account, returns true
+		if (makeWithdrawal(x) && transferTo != nullptr){
+			transferTo.makeDeposit(x);
+			return true;
+		}
+
+		//customer is unable to transfer the funds, returns false
+		cout << "Unable to transfer funds." << endl;
+		return false;
+	}
 };
 
 #endif /* main_h */
